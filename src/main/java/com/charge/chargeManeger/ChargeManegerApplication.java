70b9304ad93fde028c462571c0d1315558b55c72
Cauã Pires <caua.soares@sefaz.pb.gov.br>
2025-11-28 15:34:29 -0300

    init

---
+++ src/main/java/com/charge/chargeManeger/ChargeManegerApplication.java
@@ -0,0 +1,13 @@
+package com.charge.chargeManeger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargeManegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargeManegerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charge/chargeManeger/ChargeManegerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charge.chargeManeger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargeManegerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
